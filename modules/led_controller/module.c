d9c1094980eb31bd7b4f358f21f4b801d70708b9
Simon Busch <morphis@gravedo.de>
2012-08-02 08:51:11 +0200

    Add module implementations for charger, led_controller and system subsystems

---
+++ modules/led_controller/module.c
@@ -0,0 +1,36 @@
+/**
+ * This file is part of nyx-crespo-modules.
+ *
+ * Copyright (C) 2012 Simon Busch <morphis@gravedo.de>
+ *
+ * nyx-crespo-modules is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libsamsung-ipc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libsamsung-ipc.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <nyx/nyx_module.h>
+
+/* Sleepd requires a led_controller module with "Default" as id. */
+NYX_DECLARE_MODULE(NYX_DEVICE_LED_CONTROLLER, "Default")
+
+nyx_error_t nyx_module_open (nyx_instance_t instance, nyx_device_t** device_ptr)
+{
+    return NYX_ERROR_NONE;
+}
+
+nyx_error_t nyx_module_close (nyx_device_t* device)
+{
+    return NYX_ERROR_NONE;
+}
+
+// vim:ts=4:sw=4:expandtab
